2491e2cf11e4ed3fa690dffd568afa92a74f22ed
Joel Mendoza <joelmendza173@gmail.com>
2019-07-29 07:23:10 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,6 @@
 def roll_call_dwarves(array) # code an argument here
   # Your code here
-  array.each{|i| puts "#{count} #{i}"}
+  array.each_with_index{|i, index| puts "#{index}. #{i}"}
 end
 
 def summon_captain_planet# code an argument here
